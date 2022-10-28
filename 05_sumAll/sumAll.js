e869eb2dffae4db83bf146734461304e8d193b64
unclebinary1001 <nzundamahlangu@gmail.com>
2022-10-28 15:16:31 -0500

    solved ex 1 to 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
-
+const sumAll = function(...range) {
+    let sum = 0
+    if (!(range[0]>=0 && range[1]>=0))
+        return "ERROR"
+    if (!(typeof range[0] == "number" && typeof range[1] == "number"))
+        return "ERROR";   
+    if (range[0] > range[1]) {
+        let small = range[0];
+        range[0] = range[1];
+        range[1] = small;
+    }
+    for (let i = range[0]; i<= range[1]; i++)
+        sum += i;
+    return sum;
 };
 
 // Do not edit below this line
