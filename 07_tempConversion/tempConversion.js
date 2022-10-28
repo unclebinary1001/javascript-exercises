0cd561c91f4615efc6263b3a0bb51d1a68ce5d23
unclebinary1001 <nzundamahlangu@gmail.com>
2022-10-28 15:37:07 -0500

    solve ex 6 and 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,11 @@
-const ftoc = function() {
-
+const ftoc = function(fTemp) {
+  let cTemp = (fTemp - 32) * 5/9;
+  return parseFloat(cTemp.toFixed(1));
 };
 
-const ctof = function() {
-
+const ctof = function(cTemp) {
+  let fTemp = cTemp * 9/5 + 32;
+  return parseFloat(fTemp.toFixed(1));
 };
 
 // Do not edit below this line
