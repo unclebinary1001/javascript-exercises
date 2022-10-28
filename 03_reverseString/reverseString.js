5430e0ab29e0cd67ab6663fedce42cf792c0c60e
unclebinary1001 <nzundamahlangu@gmail.com>
2022-10-28 15:18:45 -0500

    ex 4 to 5

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let reverseString = "";
+    for (let i = string.length - 1; i>=0; i--)
+        reverseString += string.charAt(i)
+    return reverseString
 };
 
 // Do not edit below this line
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,9 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, ...remove) {
+    for (const val of remove) {
+        if (arr.indexOf(val) >= 0)
+            arr.splice(arr.indexOf(val), 1);
+    }
+    return arr
 };
 
 // Do not edit below this line
